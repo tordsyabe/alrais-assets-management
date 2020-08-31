ca0d674147472f135ccd77ca7f407c16079efc0f
John Llave <extordsllave@gmail.com>
2020-08-31 13:48:28 +0400

    added status entity

---
+++ src/main/java/com/alraisent/assetsmanagement/entity/Status.java
@@ -0,0 +1,14 @@
+package com.alraisent.assetsmanagement.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "status")
+public class Status {
+}
