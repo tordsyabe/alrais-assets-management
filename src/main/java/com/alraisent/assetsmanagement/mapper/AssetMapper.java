9c260f668cbb905237e1f706f8d18abf4de410bb
John Llave <extordsllave@gmail.com>
2020-09-20 16:47:15 +0400

    fixed AssetMapper for returning manufacturer response

--- src/main/java/com/alraisent/assetsmanagement/mapper/AssetMapper.java
+++ src/main/java/com/alraisent/assetsmanagement/mapper/AssetMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+@Mapper(uses = {LocationMapper.class, StatusMapper.class, ModelMapper.class, }, unmappedSourcePolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface AssetMapper {
 
     AssetMapper INSTANCE = Mappers.getMapper(AssetMapper.class);
